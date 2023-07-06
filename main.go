5a1fa5ac0c02dd99a55b421d7ecae70722ce691a
TomNelsonTembo <neltembo2002@gmail.com>
2023-07-06 18:35:18 +0200

    Modified main.go

--- main.go
+++ main.go
@@ -1,10 +1,27 @@
 package main
 
-import "fmt"
-
+import ("fmt"
+	"log"
+	"net/http"
+)
+func helloHandler(w http.ResponseWriter, r *http.Request){
+if r.URL.Path != "/hello" {
+	http.Error(w, "404 not found", http.StatusNotFound)
+	return
+}
+if r.Method != "GET"{
+	http.Error(w, "method is not supported". http.StatusNotFound)
+	return
+}
+}
 func main (){
-	var i int = 2
-	var j int = 4
-
-	fmt.Println(i*j)
+	fileserver := http.FileServer(http.Dir("./static"))
+	http.Handle("/", fileserver)
+	http.HandeFunc("/form", formHandler)
+	http.HandleFunc("/hello", helloHandler)
+	
+	fmt.Printf("Starting Server at port 8080\n")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
 }
